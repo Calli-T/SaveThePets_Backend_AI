297c902fa10c7e5893397076d8ce4d5a2646ce20
Calli-T <Calli-T@naver.com>
2023-10-14 18:31:54 +0900

    Switch to ViT Version

--- AI/breed_classification/views.py
+++ AI/breed_classification/views.py
@@ -31,12 +31,13 @@ from io import BytesIO
 # ------------------------------------------------------------------------------------------------------------------------------
 root = os.getcwd()
 
-img_size = (331, 331, 3)
+img_size = (448, 448, 3)
 dog_model = keras.models.load_model(
     os.path.join(root, 'breed_classification', 'models', 'dog_model.h5'))
 cat_model = keras.models.load_model(
     os.path.join(root, 'breed_classification', 'models', 'cat_model.h5'))
 
+'''
 dog_breeds = ['australian kelpie', 'cardigan welsh corgi', 'chesapeake bay retriever', 'chihuahua', 'chow chow',
               'clumber', 'cocker spaniel', 'curly-coated retriever', 'dalmatian', 'dandie dinmont', 'doberman pinscher',
               'english foxhound', 'english setter', 'english springer spaniel', 'entlebucher', 'eskimo dog',
@@ -54,21 +55,118 @@ dog_breeds = ['australian kelpie', 'cardigan welsh corgi', 'chesapeake bay retri
               'standard poodle', 'standard schnauzer', 'sussex spaniel', 'tibetan mastiff', 'tibetan terrier',
               'toy poodle', 'vizsla', 'weimaraner', 'welsh springer spaniel', 'west highland white terrier', 'whippet',
               'wire-haired fox terrier', 'yorkshire terrier']
-
-cat_breeds = ['abyssinian cat', 'american shorthair cat', 'balinese cat', 'bengal cat', 'birman cat', 'bombay cat',
+              
+              cat_breeds = ['abyssinian cat', 'american shorthair cat', 'balinese cat', 'bengal cat', 'birman cat', 'bombay cat',
               'british longhair cat', 'british shorthair cat', 'chartreux cat', 'devon rex cat', 'havana brown cat',
               'highland fold cat', 'japanese bobtail cat', 'maine coon cat', 'manx cat', 'munchkin cat', 'nebelung cat',
               'norway forest cat', 'persian cat', 'persian chinchilla cat', 'ragdoll cat', 'russian blue cat',
               'savannah cat', 'scottish fold cat', 'selkirk rex cat', 'siamese cat', 'siberian cat', 'singapura cat',
               'snowshoe cat', 'somali cat', 'sphynx cat', 'tonkinese cat', 'turkish angora cat']
+'''
+dog_breeds = ['bishon_frise', 'chihuahua', 'chow_chow', 'dalmatian', 'doberman_pinscher', 'golden_retriever',
+              'pomeranian', 'poodle', 'pug', 'siberian_husky', 'welsh_corgi', 'yorkshire_terrier']
+
+cat_breeds = ['bengal_cat', 'bombay_cat', 'british_shorthair_cat', 'ragdoll_cat', 'russian_blue_cat', 'siamese_cat',
+              'sphynx_cat']
 
 # print(os.path.join(root, 'breed_classification', 'pics'))
 
+
+'''
 # 전처리기
 nasnet_preprocessor = keras.applications.nasnet.preprocess_input
 xception_preprocessor = keras.applications.inception_resnet_v2.preprocess_input
 inception_preprocessor = keras.applications.inception_resnet_v2.preprocess_input
 inc_resnet_preprocessor = keras.applications.inception_resnet_v2.preprocess_input
+'''
+
+# ------stack_VIT(feature model)---------------------------------------------------------------------------------------------------------------------
+from keras.applications import InceptionResNetV2, EfficientNetB3
+from vit_keras import vit
+import keras
+
+from keras import Model, layers
+from keras.layers import Concatenate, RandomZoom
+
+# from env_var import *
+specie_name = 'cat'  # 'dog'
+dog_breeds = ['bishon_frise', 'chihuahua', 'chow_chow', 'dalmatian', 'doberman_pinscher', 'golden_retriever',
+              'pomeranian', 'poodle', 'pug', 'siberian_husky', 'welsh_corgi', 'yorkshire_terrier']
+
+cat_breeds = ['bengal_cat', 'bombay_cat', 'british_shorthair_cat', 'ragdoll_cat', 'russian_blue_cat', 'siamese_cat',
+              'sphynx_cat']
+IMAGE_SIZE = (448, 448, 3)
+
+models_search = {
+    'InceptionResNetV2': [InceptionResNetV2, keras.applications.inception_resnet_v2.preprocess_input],
+    'EfficientNetB3': [EfficientNetB3, keras.applications.efficientnet.preprocess_input],
+    'vit_l32': [vit.vit_l32, vit.preprocess_inputs],
+}
+models_for_stacking = ['vit_l32', 'EfficientNetB3', 'InceptionResNetV2']
+
+# 이미지에 노이즈를 주어 증강, augmentation
+data_augmentation = keras.Sequential([
+    layers.RandomFlip("horizontal"),
+    layers.RandomRotation(0.05),
+    RandomZoom(0.1),
+], name='data_augmentation')
+
+breeds_count = 12
+
+
+def get_vit_model_feat(app_class, shape, inputs, prep_input_fx):
+    base_model = app_class(
+        image_size=shape[:2],  # 이미지 가로세로
+        activation='softmax',
+        pretrained=True,
+        include_top=False,
+        pretrained_top=False,
+        classes=breeds_count
+    )
+    x = prep_input_fx(inputs)  # 전처리
+    outputs = base_model(x)  # 한걸 모델에 넣어 output
+    return outputs
+
+
+def get_keras_model_feat(app_class, shape, inputs, prep_input_fx):
+    base_model = app_class(
+        include_top=False,
+        weights='imagenet',  # ImageNet으로 학습한 가중치를 이용하는 모델들과 관련? https://keras.io/ko/applications/
+        input_shape=shape,
+    )
+    x = prep_input_fx(inputs)
+    x = base_model(x)
+    outputs = keras.layers.GlobalAveragePooling2D()(x)
+    return outputs
+
+
+def build_feat_model(models_names, shape, aug_layer=None):
+    all_outputs = []
+    inputs = keras.Input(shape=shape)
+    if aug_layer != None:
+        aug_inputs = aug_layer(inputs)
+    else:
+        aug_inputs = inputs
+    for model_type in models_names:
+        model_class = models_search[model_type][0]  # 모델?
+        model_prep_input = models_search[model_type][1]  # 모델별 이미지 전처리기?
+        if model_type.startswith('vit'):
+            model_outputs = get_vit_model_feat(model_class, shape, aug_inputs, model_prep_input)
+        else:
+            model_outputs = get_keras_model_feat(model_class, shape, aug_inputs, model_prep_input)
+        all_outputs.append(model_outputs)
+    concat_outputs = Concatenate()(all_outputs)
+    model = Model(inputs, concat_outputs)
+    return model
+
+
+def get_feature_model():
+    return build_feat_model(models_for_stacking, IMAGE_SIZE, data_augmentation)
+
+
+dog_feature_model = get_feature_model()
+breeds_count = 7
+cat_feature_model = get_feature_model()
 
 # ------------------------------------------------------------------------------------------------------------------------------
 if platform.system() == 'Windows':
@@ -76,7 +174,9 @@ if platform.system() == 'Windows':
     os.environ["PATH"] = LOCATION + ";" + os.environ["PATH"]  # 환경변수 등록, ec2에 올리려면 따로 지정해줘야할듯
 
 # 연결
-con = cx_Oracle.connect("scott", "tiger", "host.docker.internal:1521/xepdb1", encoding="UTF-8")
+# con = cx_Oracle.connect("scott", "tiger", "host.docker.internal:1521/xepdb1", encoding="UTF-8")
+con = cx_Oracle.connect("scott", "tiger", "127.0.0.1:1521/xepdb1", encoding="UTF-8")
+
 cursor = con.cursor()
 
 
@@ -106,11 +206,9 @@ def images_to_array(data_dir):
     os.chdir(data_dir)
     images_names = os.listdir(data_dir)
     test_size = len(images_names)
-    # print(images_names)
 
     X = np.zeros([test_size, img_size[0], img_size[1], 3], dtype=np.uint8)
 
-    # for i in tqdm(range(test_size)):
     for i in tqdm(range(test_size)):
         image_name = images_names[i]
         img_dir = os.path.join(data_dir, image_name)
@@ -120,6 +218,7 @@ def images_to_array(data_dir):
     return X
 
 
+'''
 def get_features(model_name, data_preprocessor, input_size, data):
     input_layer = Input(input_size)
     preprocessor = Lambda(data_preprocessor)(input_layer)
@@ -130,8 +229,28 @@ def get_features(model_name, data_preprocessor, input_size, data):
     feature_maps = feature_extractor.predict(data, batch_size=64, verbose=1)
 
     return feature_maps
+'''
 
 
+def get_features_from_model(model, ds, count=1):
+    features = []
+
+    for i in range(count):
+        predictions = model.predict(ds)
+        features.append(predictions)
+
+    return np.concatenate(features)
+
+
+def gen_vit_keras_test_feature(pics_array, specie_name='dog'):
+    # model_features = get_feature_model()
+    if specie_name == 'dog':
+        return get_features_from_model(dog_feature_model, pics_array, count=1)
+    else:
+        return get_features_from_model(cat_feature_model, pics_array, count=1)
+
+
+'''
 def gen_test_features(pics_array):
     # 전처리기 원래 위치
 
@@ -145,6 +264,7 @@ def gen_test_features(pics_array):
                            xception_features_test,
                            inception_features_test,
                            inc_resnet_features_test], axis=-1)
+'''
 
 
 def predict(features, model):
@@ -156,7 +276,7 @@ def cos_sim(A, B):
     return dot(A, B) / (norm(A) * norm(B))
 
 
-def setDBSimilarity(post_id, post_type):
+def setDBSimilarity(post_id, post_type, species='1'):
     sql = f"select picture, post_id from postpictures where not post_id = {post_id} and (select type from posts where post_id = postpictures.post_id) = {post_type}"
     res = cursor.execute(sql)
     urls = []
@@ -182,7 +302,7 @@ def setDBSimilarity(post_id, post_type):
         response = requests.get(url)
         images.append(Image.open(BytesIO(response.content)))
 
-    images = [image.resize((331, 331)) for image in images]
+    images = [image.resize((448, 448)) for image in images]  # (331, 331)
     images = np.array([np.array(image) for image in images])
 
     post_images = []
@@ -190,11 +310,18 @@ def setDBSimilarity(post_id, post_type):
         response = requests.get(url)
         post_images.append(Image.open(BytesIO(response.content)))
 
-    post_images = [image.resize((331, 331)) for image in post_images]
+    post_images = [image.resize((448, 448)) for image in post_images]  # (331, 331)
     post_images = np.array([np.array(image) for image in post_images])
 
-    post_feature_vector = gen_test_features(post_images)
-    feature_vector = gen_test_features(images)
+    post_feature_vector = []  # gen_vit_keras_test_feature(post_images, 'dog')  # gen_test_features(post_images)
+    feature_vector = []  # gen_vit_keras_test_feature(images, 'dog')  # gen_test_features(images)
+
+    if species == 1:
+        post_feature_vector = gen_vit_keras_test_feature(post_images, 'dog')
+        feature_vector = gen_vit_keras_test_feature(images, 'dog')
+    elif species == 0:
+        post_feature_vector = gen_vit_keras_test_feature(post_images, 'cat')
+        feature_vector = gen_vit_keras_test_feature(images, 'cat')
 
     for i in range(len(urls)):
         for j in range(len(post_feature_vector)):
@@ -228,10 +355,14 @@ def get_breed_with_post_id(post_id, species=1):
         response = requests.get(url)
         images.append(Image.open(BytesIO(response.content)))
 
-    images = [image.resize((331, 331)) for image in images]
+    images = [image.resize((448, 448)) for image in images]
     images = np.array([np.array(image) for image in images])
 
-    test_images_features = gen_test_features(images)
+    test_images_features = []
+    if species == 1:
+        test_images_features = gen_vit_keras_test_feature(images, 'dog')  # gen_test_features(images)
+    else:
+        test_images_features = gen_vit_keras_test_feature(images, 'cat')
     y_pred = []
     if species == 1:
         y_pred = predict(test_images_features, dog_model)
@@ -256,6 +387,7 @@ def get_breed_with_post_id(post_id, species=1):
             best_breed = breed
 
     return best_breed
+
 
 # ------------------------------------------------------------------------------------------------------------------------------
 @api_view(['GET'])
@@ -321,17 +453,37 @@ def Breed_classify(request):
             response = requests.get(url)
             images.append(Image.open(BytesIO(response.content)))
 
-        images = [image.resize((331, 331)) for image in images]
+        images = [image.resize((448, 488)) for image in images]  # (331, 331)
         images = np.array([np.array(image) for image in images])
 
-        test_images_features = gen_test_features(images)
+        print(res, type(res))
 
-        y_pred = predict(test_images_features, dog_model)
-        ans = []
-        for i in range(len(y_pred)):
-            ans.append(f'{urls[i]} : {dog_breeds[np.argmax(y_pred[i])]}')
+        sql = f'select species from posts where post_id = {str(post_id)}'
+        res = cursor.execute(sql)
+        species = -1
+        for line in res:
+            species = line[0]
 
-        return Response(ans)
+        test_images_features = []
+        if species == 1:
+            test_images_features = gen_vit_keras_test_feature(images, 'dog')  # gen_test_features(images)
+        elif species == 0:
+            test_images_features = gen_vit_keras_test_feature(images, 'cat')
+
+        if species == 1:
+            y_pred = predict(test_images_features, dog_model)
+            ans = []
+            for i in range(len(y_pred)):
+                ans.append(f'{urls[i]} : {dog_breeds[np.argmax(y_pred[i])]}')
+
+            return Response(ans)
+        elif species == 0:
+            y_pred = predict(test_images_features, cat_model)
+            ans = []
+            for i in range(len(y_pred)):
+                ans.append(f'{urls[i]} : {cat_breeds[np.argmax(y_pred[i])]}')
+
+            return Response(ans)
 
     # 사진의 분류들중 가장 많은수를 확인해야한다.
     elif request.method == 'POST':
@@ -351,12 +503,17 @@ def Breed_classify(request):
             images = [Image.open(image) for image in images]
 
             # 변환된 이미지를 331x331 크기의 이미지로 변환후 np 배열로 바꿈
-            images = [image.resize((331, 331)) for image in images]
+            images = [image.resize((448, 448)) for image in images]
             images = np.array([np.array(image) for image in images])
 
             # 배열로 특징 벡터를 뽑고 모델에 넣어 나온 값으로 추정
 
-            test_images_features = gen_test_features(images)
+            test_images_features = []
+            if species == 1:
+                test_images_features = gen_vit_keras_test_feature(images, 'dog')  # gen_test_features(images)
+            elif species == 0:
+                test_images_features = gen_vit_keras_test_feature(images, 'cat')
+
             y_pred = []
             if species == 1:
                 y_pred = predict(test_images_features, dog_model)
@@ -387,6 +544,13 @@ def Breed_classify(request):
 def Image_Similarity(request):
     if request.method == 'POST':
         post_id = request.data.get('post_id')
-        best = setDBSimilarity(post_id)
+
+        sql = f'select species from posts where post_id = {str(post_id)}'
+        res = cursor.execute(sql)
+        species = -1
+        for line in res:
+            species = line[0]
+
+        best = setDBSimilarity(post_id, species)
 
         return Response(best)
